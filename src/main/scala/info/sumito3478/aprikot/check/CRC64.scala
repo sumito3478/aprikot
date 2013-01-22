86bfaf9a6a069b07ae64b1890e218376e03ad03f
sumito3478 <sumito3478@gmail.com>
2013-01-22 19:28:20 +0900

    Add CRC64 implementations and specs

--- src/main/scala/info/sumito3478/aprikot/check/CRC64.scala
+++ src/main/scala/info/sumito3478/aprikot/check/CRC64.scala
@@ -1,5 +1,131 @@
+/* Copyright (C) 2013 sumito3478 <sumito3478@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Note:
+ * This implementation is a port to Scala from the public domain implementation
+ * written in C.
+ * I took that C implementation from XZ Utils (actually, most part of XZ Utils
+ * is publish domain):
+ * - src/liblzma/check/crc_macros.h
+ * - src/liblzma/check/crc64_tablegen.c
+ * - src/liblzma/check/crc64_fast.c
+ * from http://git.tukaani.org/xz.git
+ * with commit 96f94bc925d579a700147fa5d7793b64d69cfc18 (HEAD of master at the
+ * time I referenced them).
+ * The author of C implementation is Lasse Collin. Thanks a lot!
+ */
+
 package info.sumito3478.aprikot.check
 
-trait CRC64 {
+import java.lang.{ Long => JLong }
+import info.sumito3478.aprikot.unsafe.{ Memory, Pointer, ByteOrder, bswap }
+import java.lang.{ Integer => JInteger }
 
+trait CRC64 {
+  def poly: Long
+
+  private[this] val rp = JLong.reverse(poly)
+
+  private[this] val table: Memory = {
+    val ret = Memory(8 * 8 * 256)
+    val p = ret.pointer
+    0 until 4 foreach {
+      s =>
+        0 until 256 foreach {
+          b =>
+            var r = if (s == 0) b else (p + ((s - 1) * 256 + b) * 8).long
+            0 until 8 foreach {
+              i =>
+                if ((r & 1) != 0) {
+                  r = (r >>> 1) ^ rp
+                } else {
+                  r >>>= 1
+                }
+                (p + (s * 256 + b) * 8).long = r
+            }
+        }
+    }
+    ret
+  }
+
+  private[this] val t = table.pointer
+
+  private[this] def tab(x: Long)(y: Long): Long = {
+    (t + (x * 256 + y) * 8).long
+  }
+
+  private[this] val be = ByteOrder.isBE
+
+  private[this] def A(x: Int) = if (be) x >>> 24 else x & 0xff
+
+  private[this] def B(x: Int) = if (be) (x >>> 16) & 0xff else (x >>> 8) & 0xff
+
+  private[this] def C(x: Int) = if (be) (x >>> 8) & 0xff else (x >>> 16) & 0xff
+
+  private[this] def D(x: Int) = if (be) x & 0xff else x >>> 24
+
+  private[this] def S8(x: Long) = if (be) x << 8 else x >>> 8
+
+  private[this] def S32(x: Long) = if (be) x << 32 else x >>> 32
+
+  private[this] def A1(x: Long) = if (be) x >>> 56 else A((x & 0xffff).toInt)
+
+  def process(x: Byte, seed: Long) = {
+    tab(0)((x & 0xff) ^ A1(seed)) ^ S8(seed)
+  }
+
+  def process(tmp: Int, seed: Long) = {
+    var crc = seed
+    crc = (tab(3)(A(tmp))
+      ^ tab(2)(B(tmp))
+      ^ S32(crc)
+      ^ tab(1)(C(tmp))
+      ^ tab(0)(D(tmp)))
+    crc
+  }
+
+  def apply(p: Pointer, len: Long, seed: Long): Long = {
+    var size = len
+    var crc = ~seed
+    crc = if (be) bswap(crc) else crc
+    var buf = p
+    if (size > 4) {
+      while ((buf.p & 3) != 0) {
+        crc = process(buf.byte, crc)
+        buf += 1
+        size -= 1
+      }
+      val limit = buf + (size & ~3)
+      size &= 3
+      while (buf.p < limit.p) {
+        val tmp = if (be) {
+          (crc >>> 32).toInt ^ buf.int
+        } else {
+          crc.toInt ^ buf.int
+        }
+        buf += 4
+        crc = process(tmp, crc)
+      }
+    }
+    while (size != 0) {
+      crc = process(buf.byte, crc)
+      buf += 1
+      size -= 1
+    }
+    crc = if (be) bswap(crc) else crc
+    ~crc
+  }
 }
---
+++ src/test/scala/info/sumito3478/aprikot/check/CRC64ECMASpec.scala
@@ -0,0 +1,63 @@
+/* Copyright (C) 2013 sumito3478 <sumito3478@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.sumito3478.aprikot.check
+import info.sumito3478.aprikot.unsafe.{ ArrayOfByteW, Memory, benchmark, LongW }
+
+import org.scalatest.FunSpec
+
+class CRC64ECMASpec extends FunSpec {
+  val test1 = "hello world".getBytes("UTF-8")
+
+  val test1ret = 0x53037ecdef2352daL
+
+  val test1buffer = {
+    val ret = Memory(test1.length + 8)
+    test1.memcpy(ret.pointer)
+    (ret.pointer + test1.length).long = (~test1ret).toLE
+    ret
+  }
+
+  val test2 =
+    "Mis, Mis, Mister, Drill, Driller, I'll do my best, I cant lose!".
+      getBytes("UTF-8")
+
+  val test2ret = 0x49dd4f8aa704fb69L
+
+  val test2buffer = {
+    val ret = Memory(test2.length + 8)
+    test2.memcpy(ret.pointer)
+    (ret.pointer + test2.length).long = (~test2ret).toLE
+    ret
+  }
+
+  describe("CRC64ECMA.apply(Pointer, Long, Long)") {
+    it("should calculate the CRC64ECMA value of test1.") {
+      assert(CRC64ECMA(test1buffer.pointer, test1.length, 0) === test1ret)
+    }
+
+    it("should return 0 if the CRC64ECMA of the test1 is appended to that.") {
+      assert((~CRC64ECMA(test1buffer.pointer, test1.length + 8, 0)) === 0)
+    }
+    it("should calculate the CRC64ECMA value of test2.") {
+      assert(CRC64ECMA(test2buffer.pointer, test2.length, 0) === test2ret)
+    }
+
+    it("should return 0 if the CRC64ECMA of test2 is appended to that.") {
+      assert((~CRC64ECMA(test2buffer.pointer, test2.length + 8, 0)) === 0)
+    }
+  }
+}
