9cf5f66f589493cea0c9a7073795e9a51208efe6
sumito3478 <sumito3478@gmail.com>
2013-02-03 17:33:48 +0900

    Bump version number to 0.1.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object build extends Build {
       libraryDependencies ++= Seq(
         "net.java.dev.jna" % "jna" % "3.5.+"
         ),
-      version := "0.0.10"
+      version := "0.1.0-SNAPSHOT"
     ): _*
   )
 
