20dad23cec68f310079991abc7cab50e6b747d54
sumito3478 <sumito3478@gmail.com>
2013-02-10 05:16:35 +0900

    Add specs for PerseusAnalysisDatum

---
+++ src/test/scala/info/sumito3478/aprikot/classic/perseus/sql/PerseusAnalysisDatumSpec.scala
@@ -0,0 +1,44 @@
+/* Copyright (C) 2013 sumito3478 <sumito3478@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.sumito3478.aprikot.classic.perseus.sql
+
+import org.scalatest.FunSpec
+
+import scala.slick.driver.BasicDriver.simple._
+import Database.threadLocalSession
+
+class PerseusAnalysisDatumSpec extends FunSpec {
+  describe("PerseusAnalysisDatum.create") {
+    it("should create new table in database") {
+      val db = Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver")
+      db withSession {
+        val datum = PerseusAnalysisDatum
+        datum.ddl.create
+        datum.insert(None, "saluto", "67838076 9 salu_to_,saluto", " ", "pres ind act 1st sg")
+        val r = Query(datum)
+        val data = r.first
+        println(data)
+        val (id, inflected, lemma, vocab, inflection) = data
+        assert(id === Some(1))
+        assert(inflected === "saluto")
+        assert(lemma === "67838076 9 salu_to_,saluto")
+        assert(vocab === " ")
+        assert(inflection === "pres ind act 1st sg")
+      }
+    }
+  }
+}
