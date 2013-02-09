9b0b949ed8251ae9ff9da8165532ea1b148e567f
sumito3478 <sumito3478@gmail.com>
2013-02-10 04:18:01 +0900

    Add more specs for AnalysisDataParser

--- src/test/scala/info/sumito3478/aprikot/classic/perseus/AnalysisDataParserSpec.scala
+++ src/test/scala/info/sumito3478/aprikot/classic/perseus/AnalysisDataParserSpec.scala
@@ -21,7 +21,7 @@ import scala.util.parsing.input.CharSequenceReader
 
 class AnalysisDataParserSpec extends FunSpec {
   describe("AnalysesDataParserSpec#Line") {
-    it("should returns AnalysesData") {
+    it("should return list of AnalysisData") {
       val l = "saluto\t{67838076 9 salu_to_,saluto\t \tpres ind act 1st sg}"
       val r = AnalysisDataParser.Line(new CharSequenceReader(l))
       assert(r.isInstanceOf[AnalysisDataParser.Success[_]] === true)
@@ -32,6 +32,34 @@ class AnalysisDataParserSpec extends FunSpec {
             new LemmaDescription("67838076 9 salu_to_,saluto"),
             new ShortVocabDescription(" "),
             new InflectionDescription("pres ind act 1st sg")))
+        case f: AnalysisDataParser.Failure => {
+          println(f)
+        }
+        case e: AnalysisDataParser.Error => {
+          println(e.msg)
+        }
+      }
+    }
+    it("""should return List of AnalysisData for the line that contains
+        |  multiple analysis datum.""".stripMargin) {
+      val l = "cerebrum\t{12844671 9 cere_bru_m,cerebrum\tthe brain\tneut gen pl}{12844671 9 cere_brum,cerebrum\tthe brain\tneut nom/voc/acc sg}"
+      val r = AnalysisDataParser.Line(new CharSequenceReader(l))
+      assert(r.isInstanceOf[AnalysisDataParser.Success[_]] === true)
+      r match {
+        case s: AnalysisDataParser.Success[_] => {
+          val xs = s.result
+          assert(xs.length === 2)
+          assert(xs(0) === new AnalysisData(
+            new InflectedWord("cerebrum"),
+            new LemmaDescription("12844671 9 cere_bru_m,cerebrum"),
+            new ShortVocabDescription("the brain"),
+            new InflectionDescription("neut gen pl")))
+          assert(xs(1) === new AnalysisData(
+            new InflectedWord("cerebrum"),
+            new LemmaDescription("12844671 9 cere_brum,cerebrum"),
+            new ShortVocabDescription("the brain"),
+            new InflectionDescription("neut nom/voc/acc sg")))
+        }
         case f: AnalysisDataParser.Failure => {
           println(f)
         }
