e1391e836d5ec7ed93c407a0167bff5f89fc5c35
sumito3478 <sumito3478@gmail.com>
2013-02-12 04:22:27 +0900

    Move LatinTextAnalysis to package classic

--- src/main/scala/info/sumito3478/aprikot/classic/LatinTextAnalysis.scala
+++ src/main/scala/info/sumito3478/aprikot/classic/LatinTextAnalysis.scala
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package info.sumito3478.aprikot.classic.perseus
+package info.sumito3478.aprikot.classic
 
 class LatinTextAnalysis {
   
