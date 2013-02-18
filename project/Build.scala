3a8c174198135f794aa917b05d6be825a6a33943
sumito3478 <sumito3478@gmail.com>
2013-02-18 12:18:34 +0900

    Bump version number to 0.4.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,7 @@ object build extends Build {
         "ch.qos.logback" % "logback-classic" % "1.0.+",
         "net.java.dev.jna" % "jna" % "3.5.+"
         ),
-      version := "0.3.0"
+      version := "0.4.0-SNAPSHOT"
     ): _*
   )
 
